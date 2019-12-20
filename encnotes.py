872bf5daf260b239f680c10d0f00a374a4078dc6
Alex <aleksandrovich@tuta.io>
2019-12-20 20:04:26 +0300

    Some changes

--- encnotes.py
+++ encnotes.py
@@ -93,9 +93,10 @@ def index():
         rnumber = random.randint(1000000, 9999999)
         while True:
             n = Note.query.filter_by(number=rnumber).first()
-            if not n:
-                break
-            rnumber = random.randint(1000000, 9999999)
+            if n:
+                rnumber = random.randint(1000000, 9999999)
+                continue
+            break            
         cipher_note = Note(number=rnumber, ciptext=str_cipher_text)
         link = f'{app.config["SITE_URL"]}/{rnumber}/{str_key}'
         db.session.add(cipher_note)
